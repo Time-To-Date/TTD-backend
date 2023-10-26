5bb7e56004269787eb8917df81e24777c963a66f
Nomik94 <kill0374@naver.com>
2023-10-26 18:35:51 +0900

    chore: graphql -> rest api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,11 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
 import { UserModule } from '@/modules/user/user.module';
 import { PrismaModule } from '@/modules/prisma/prisma.module';
 import { AuthModule } from '@/modules/auth/auth.module';
-import { AuthResolver } from '@/modules/auth/auth.resolver';
-import { AuthService } from '@/modules/auth/auth.service';
 import { LoggerMiddleware } from '@/common/middleware/logger.middleware';
 
 @Module({
-  imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/common/graphql/schema.gql'),
-      sortSchema: true,
-      installSubscriptionHandlers: true,
-      buildSchemaOptions: { dateScalarMode: 'timestamp' },
-    }),
-    UserModule,
-    PrismaModule,
-    AuthModule,
-  ],
-  controllers: [],
-  providers: [AuthResolver, AuthService],
+  imports: [UserModule, PrismaModule, AuthModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
